f5eb71de6e27370babcc2aa4999db11e139cde34
Omar Alshafei <omaralshafei02@gmail.com>
2023-09-02 18:58:15 -0400

    fixed syntax

--- LAMPAPI/CreateContact.php
+++ LAMPAPI/CreateContact.php
@@ -1,46 +1,40 @@
 <?php
 	$inData = getRequestInfo();
-
     
-	$UserId = $inData["UserId"];
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$Phone = $inData["Phone"];
-	$Email = $inData["Email"];
-
+	$userId = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
+	if ($conn->connect_error) {
 		returnWithError( $conn->connect_error );
 	} 
-	else
-	{
+	else {
 		$stmt = $conn->prepare("INSERT INTO Contacts (UserId, FirstName, LastName, Phone, Email) VALUES (?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssss", $UserId, $FirstName, $LastName, $Phone, $Email);
+		$stmt->bind_param("sssss", $userId, $firstName, $lastName, $phone, $email);
         if ($stmt->execute()) {
             echo "Contact added successfully!";
         }
         else {
             echo "Error: " . $stmt->error;
         }
+		
 		$stmt->close();
 		$conn->close();
 	}
 
-	function getRequestInfo()
-	{
+	function getRequestInfo() {
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
+	function sendResultInfoAsJson( $obj ) {
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
-	{
+	function returnWithError( $err ) {
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -2,28 +2,24 @@
 
     $inData = getRequestInfo();
     
-    $ID = 0;
-    $FirstName = "";
-    $LastName = "";
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");    
-    if( $conn->connect_error )
-    {
+    if( $conn->connect_error ) {
         returnWithError( $conn->connect_error );
     }
-    else
-    {
+    else {
         $stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE Login=? AND Password=?");
         $stmt->bind_param("ss", $inData["login"], $inData["password"]);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if( $row = $result->fetch_assoc()  )
-        {
+        if( $row = $result->fetch_assoc() ) {
             returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
         }
-        else
-        {
+        else {
             returnWithError("No Records Found");
         }
 
@@ -31,26 +27,22 @@
         $conn->close();
     }
     
-    function getRequestInfo()
-    {
+    function getRequestInfo() {
         return json_decode(file_get_contents('php://input'), true);
     }
 
-    function sendResultInfoAsJson( $obj )
-    {
+    function sendResultInfoAsJson( $obj ) {
         header('Content-type: application/json');
         echo $obj;
     }
     
-    function returnWithError( $err )
-    {
-        $retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
+    function returnWithError( $err ) {
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
         sendResultInfoAsJson( $retValue );
     }
     
-    function returnWithInfo( $FirstName, $LastName, $ID )
-    {
-        $retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
+    function returnWithInfo( $firstName, $lastName, $id ) {
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
         sendResultInfoAsJson( $retValue );
     }
     
